68beb97cb8bc334000560f2576168b0ab275e574
JangByeongHui <jangbyeonghui@JangByeongHuiui-MacBookPro.local>
2021-09-11 15:39:46 +0900

    feat: change library style

--- main.cpp
+++ main.cpp
@@ -7,57 +7,20 @@ Motor mt(D11,PC_8);
 
 #define COOLER 1
 #define HEAT 2
+//Button
+DigitalIn SW2(PA_14); //left button
+DigitalIn SW9(PB_7); // center button
+DigitalIn SW10(PC_4);// right button
 
-void delay(uint32_t time);
-void config();
-void RCC_Config();
-void GPIO_Config();
-void TIM_Config();
-void ultrasonic();
-void bluetooth();
-void left_led_on();
-void left_led_off();
-void toggle_mode();
-void center_led_on();
-void center_led_off();
-void toggle_center_led();
-void right_led_on();
-void right_led_off();
-void toggle_right_led();
-void print_led(int result);
-void display();
-void rgb_red_on();//RGB RED 
-void rgb_red_off();
-void rgb_blue_on(); //RGB BLUE
-void rgb_blue_off();
-void rgb_green_on();//RGB GREEN
-void rgb_green_off();
+//LED
+DigitalOut D7(PA_13); //left led
+DigitalOut D8(D6); //center led
+DigitalOut D9(PA_4); //right led
 
-void RGB_OFF();
-void SYSTEM_OFF();
-void SYSTEM_ON();
-void WIND_ON(int wind_power);
-void WIND_OFF();
-
-bool is_pressed_SW2(){
-   // PA14 SW2
-   if((GPIOA->IDR & (1<<14)) == 0) return true;
-   else return false;
-}
-
-bool is_pressed_SW9(){
-   // PB7 SW9
-   if((GPIOB->IDR & (1<<7)) == 0) return true;
-   else return false;
-}
-
-bool is_pressed_SW10(){
-   // PC4 SW10
-   if((GPIOC->IDR & (1<<4)) == 0) return true;
-   else return false;
-}
-
-
+//RGB
+DigitalOut Red(A1); //RGB Red
+DigitalOut Green(PC_6); //RGB Green
+DigitalOut Blue(A3);; //RGB Blue
 
 class I2CPreInit : public I2C{
    public:
@@ -76,205 +39,21 @@ DigitalIn echo_pin(D7);
 
 HCSR04 hcsr(D10, D7);
 
+Timeout myTimeout;
 
 bool sys_on=false;
 int mode=COOLER; //COOLER, HEAT => default COOLER
-float current_temp, desired_temp=18.0; //default = 18.0; 
-int wind_power=1; // 1~5
+float current_temp=26.8, desired_temp=18.0; //default = 18.0; 
+int wind_power=0.5; // 0.0~1.0
 bool automatic=true;
-Ticker display_ticker;
-
-int main(){
-	myGUI.clearDisplay();
-  config();
-	
-	display_ticker.attach(&display,1.0);	
-	pc.printf("alskdjf\r\n");
-  while(1)
-  { 
-			current_temp=26.8;
-			WIND_ON(wind_power);
-		// click SW2 : left button = cooler / heater
-      if((GPIOA->IDR & (1<<14)) == 0){
-				if(sys_on){
-					toggle_mode();
-					rgb_red_off();
-					rgb_green_off();
-					rgb_blue_on();
-				}
-			}
-			//click SW9 : center button = auto / manual
-			if((GPIOB->IDR & (1<<7)) == 0){
-				if(sys_on){
-					pc.printf("center button\r\n");
-					toggle_center_led();
-					rgb_red_off();
-					rgb_blue_off();
-					rgb_green_on();
-				}
-			}
-			//click SW 10 : right button = power on/off
-			if((GPIOC->IDR & (1<<4)) == 0){
-				if(sys_on){
-					pc.printf("air conditioner power off\r\n");
-					SYSTEM_OFF();
-				}
-				else{
-					pc.printf("air conditioner power on\r\n");
-					rgb_blue_off();
-					rgb_green_off();
-					rgb_red_on();
-					SYSTEM_ON();
-				}
-			}   
-			wait(0.5);
-  }
-}
-
-void config(){
-  pc.baud(9600);
-  echo_pin.mode(PullDown); //ultrasonic
-	//RCC_Config();
-	GPIO_Config();
-	//TIM_Config();
-	//RGB_OFF();
-}
-
-void ultrasonic(){
-      hcsr.start();
-      wait_ms(300);
-      printf("Distance(cm): %d\r\n", hcsr.get_dist_cm());
-}
-
-   
-void delay(uint32_t time){
-   //depend on hardware clock cycle, caculate second time
-   while(time--);
-}
-
-void RCC_Config(){
-   RCC->CR   |=   (1<<16);            // HSE ON
-  while(!(RCC->CR   &(1<<17)));   // HSE ready waiting
-  RCC->CFGR   |=   (1<<12);         // APB1 PSC=0
-  RCC->CFGR   &=   ~(7<<13);         // APB2 PSC=0
-  RCC->CFGR   |=   (10<<4);              // AHB   PSC=8
-  RCC->PLLCFGR   &=   ~(0xFFFFFFFF);   // APB2 PSC=0
-  RCC->PLLCFGR   |=   (1<<22);   //   HSE selected
-  RCC->PLLCFGR   &=   ~(3<<16);   // PLLP=2
-  RCC->PLLCFGR   |=   (64<<6);   // PLLN=64
-  RCC->PLLCFGR   |=   (1<<2);      // PLLM=4
-  RCC->PLLCFGR   |=   (1<<29);   // PLLR=2
-  RCC->PLLCFGR   |=   (1<<25);   // PLLQ=2
-   
-  RCC->CR   |=   (1<<24);            // PLL ON
-  while(!(RCC->CR   &(1<<25)));   // PLL ready waiting   
-}
-
-void GPIO_Config(){
-  //LED : PA13(PA13), D6(PB10), A2(PA4)   => bit print
-  //Button :PA14(+1) , PB7(print) : , PC4
-   //RGB: Red A1(PA1), Green PC6(PC6), Blue A3(PB0)
-   
-   
-   RCC->AHB1ENR |= (1<<0); // GPIOA clock enable
-   RCC->AHB1ENR |= (1<<1); // GPIOB clock enable
-   RCC->AHB1ENR |= (1<<2); // GPIOC clock enable
-   
-		//PA13 LED : [27th:26th] => [0:1] => output mode
-		GPIOA->MODER &= ~(1<<27);
-		GPIOA->MODER |= (1<<26);
-		
-		//D6 LED => PB10 
-		GPIOB->MODER &= ~(1<<21);
-		GPIOB->MODER |= (1<<20);
-		
-		//A2 LED =>PA4
-		GPIOA->MODER &= ~(1<<9);
-		GPIOA->MODER |= (1<<8);
-		
-		//PA14 SWITCH
-		GPIOA->MODER &= ~(1<<29);
-		GPIOA->MODER &= ~(1<<28);
-		
-		//PB7 SWITCH
-		GPIOB->MODER  &= ~(1<<15);
-		GPIOB->MODER  &= ~(1<<14);
-		
-		//PC4 
-		GPIOC->MODER &= ~(1<<9);
-		GPIOC->MODER &= ~(1<<8);	
-	
-					 
-		 //RGB RED PA1 Output mode
-		 GPIOA->MODER &=~(1<<3);
-		 GPIOA->MODER |=~(1<<2);
-
-		 //RGB GREEN PB0 Output mode
-		 GPIOB->MODER &= ~(1<<1);
-		 GPIOB->MODER |= (1<<0); 
-
-		 //RGB GREEN PC6 Output mode
-		 GPIOC->MODER &= ~(1<<13);
-		 GPIOC->MODER |= (1<<12); 
-
-	
-		GPIOA->OTYPER=0;
-		GPIOA->OSPEEDR=0;
-		GPIOB->OTYPER=0;
-		GPIOB->OSPEEDR=0;
-		GPIOC->OTYPER=0;
-		GPIOC->OSPEEDR=0;
-   
-}
-
-
-void left_led_on(){
-   GPIOA->ODR |= (1<<13);         
-}
-void left_led_off(){
-   GPIOA->ODR &= ~(1<<13);
-}
-void toggle_mode(){
-	
-	
-	if(GPIOA->ODR &(1<<13) == 0){
-		pc.printf("Heater on\r\n");
-      GPIOA->ODR |= (1<<13); 
-		mode= HEAT;
-	}else if(GPIOA->ODR & (1<<13)==1){
-		pc.printf("Cooler on\r\n");
-      GPIOA->ODR &= ~(1<<13);
-		mode = COOLER;
-	}	
-}
-
-void center_led_on(){
-   GPIOB->ODR |= (1<<10);
-}
-void center_led_off(){
-   GPIOB->ODR &= ~(1<<10);
-}
-void toggle_center_led(){
-   if(GPIOB->ODR &(1<<10)==0) GPIOB->ODR |= (1<<10);
-	else GPIOB->ODR &= ~(1<<10);
-}
-void right_led_on(){
-   GPIOA->ODR |= (1<<4);         
-}
-void right_led_off(){
-   GPIOA->ODR &= ~(1<<4);
-}
-void toggle_right_led(){
-   if(GPIOA->ODR &(1<<4)==0) GPIOA->ODR |= (1<<4);
-	else GPIOA->ODR &= ~(1<<4);
-}
+bool ultrasonic_timeout_flag = false;
 
 void display(){
-	//pc.printf("displayed\r\n");
    myGUI.clearDisplay();
    myGUI.setTextCursor(0,0);
-		wait(0.1);
+	wait(0.1);
    //system on/off
+
    if(sys_on){
       myGUI.printf ("HVAC power is on\r\n");
 		 //conditioner mode COOLER/HEATER
@@ -308,51 +87,116 @@ void display(){
    //myGUI.printf ("%ux%u OLED Display\r\n",myGUI.width(), myGUI.height());
    myGUI.display();
 }
-
-void rgb_red_on(){
-    GPIOA->ODR |=(1<<1);
-}
-void rgb_red_off(){
-    GPIOA->ODR &=~(1<<1);
-}
-void rgb_blue_on(){
-    GPIOB->ODR |=(1<<0);
-}
-void rgb_blue_off(){
-    GPIOB->ODR &=~(1<<0);
-}
-void rgb_green_on(){
-    GPIOC->ODR |=(1<<6);
-}
-void rgb_green_off(){
-    GPIOC->ODR &=~(1<<6);
-}
+int main(){
+	myGUI.clearDisplay();
+   pc.baud(9600);
+   echo_pin.mode(PullDown); //ultrasonic
+	
+   while(1)
+   {
 
 
-void SYSTEM_OFF(){
-	right_led_off();
-	sys_on = !sys_on;
-	left_led_off();
-	mode=HEAT;
-	center_led_off();
-	automatic=false;
+      while(sys_on){
+         // click SW2 : left button = cooler / heater
+         if(!SW10){
+            if(mode==COOLER){
+               Red=1;
+               Blue=0;
+               Green=0;
+               mt.backward(wind_power);
+
+            }
+            else{
+               Red=0;
+               Blue=1;
+               Green=0;
+               mt.forward(wind_power);
+            }
+
+			}
+			//click SW9 : center button = auto / manual
+		   if(!SW9){
+            if(automatic)
+            {
+               automatic=false;
+               D8=0;
+            }
+            else{
+               automatic=true;
+               D8=1;
+            }
+			}
+         if(automatic){
+
+         }
+         else{
+
+         }
+			//click SW 10 : right button = power on/off
+		   if(!SW10){
+            sys_on!=sys_on;
+            if(!sys_on){
+               D9=0;
+               D8=0;
+               D7=0;
+               Red=0;
+               Blue=0;
+               Green=1;
+               break;
+            }else{
+               D9=1;
+               D8=1;
+               D7=1;
+               Red=0;
+               Blue=1;
+               Green=0;
+               mt.forward(wind_power);
+            }
+
+			}
+
+      }
+		   
+			wait(0.5);
+  }
 }
-void SYSTEM_ON(){
-	right_led_on();
-	sys_on = !sys_on;
-	left_led_on();
-	mode=COOLER;
-	center_led_on();
-	automatic=true;
+void ultrasonic(){
+   // pc.printf("ultrasonic() started...\r\n");
+   
+   hcsr.start();
+   wait_ms(500);
+   int distance = hcsr.get_dist_cm();
+   pc.printf("Distance(cm): %d\r\n", distance);
+   
+   if (distance >= 50 && !ultrasonic_timeout_flag){
+      //pc.printf("[1] distance >= 50 && timeout is NOT activated\r\n");
+      myTimeout.attach(&timeout_SYSTEM_OFF, 3.0);
+      ultrasonic_timeout_flag = true;
+      //pc.printf("timeout attach\r\n");
+   }
+   else if (distance >= 50 && ultrasonic_timeout_flag){
+      //pc.printf("[2] distance >= 50 && timeout is activated\r\n");
+      // pc.printf("timeout already attached\r\n");
+   }
+   else if (distance < 50 && !ultrasonic_timeout_flag){
+      //pc.printf("[3] distance < 50 && timeout is NOT activated\r\n");
+   }
+   else if (distance < 50 && ultrasonic_timeout_flag){
+      //pc.printf("[4] distance < 50 && timeout is activated\r\n");
+      myTimeout.detach();
+      ultrasonic_timeout_flag = false;
+      //pc.printf("timeout detach\r\n");
+   }
+   
+   wait(0.5);
 }
 
-void WIND_ON(int wind_power){
-	if(mode==COOLER){
-		mt.forward(float(wind_power/5));
-	}else{
-		mt.backward(float(wind_power/5));
-	}
+void timeout_SYSTEM_OFF(){
+    pc.printf("TIMEOUT! task activated\r\n");
+    // SYSTEM_OFF();
+      ultrasonic_timeout_flag = false;
 }
-void WIND_OFF(){
-	mt.stop();
-}
+
+
+
+   
