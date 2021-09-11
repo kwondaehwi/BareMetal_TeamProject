74ce95a761a5bf48487442d281a3c92ba4e5d796
dmstmdrbs <dmstmdrbs98@gmail.com>
2021-09-11 14:49:31 +0900

    Delete toggle function

--- main.cpp
+++ main.cpp
@@ -80,7 +80,7 @@ HCSR04 hcsr(D10, D7);
 bool sys_on=false;
 int mode=COOLER; //COOLER, HEAT => default COOLER
 float current_temp, desired_temp=18.0; //default = 18.0; 
-int wind_power=1; // 1~5
+int wind_power=2; // 1~5
 bool automatic=true;
 Ticker display_ticker;
 
@@ -89,11 +89,10 @@ int main(){
   config();
 	
 	display_ticker.attach(&display,1.0);	
-	pc.printf("alskdjf\r\n");
+	pc.printf("HAVC SYSTEM\r\n");
   while(1)
   { 
 			current_temp=26.8;
-			WIND_ON(wind_power);
 		// click SW2 : left button = cooler / heater
       if((GPIOA->IDR & (1<<14)) == 0){
 				if(sys_on){
@@ -107,7 +106,14 @@ int main(){
 			if((GPIOB->IDR & (1<<7)) == 0){
 				if(sys_on){
 					pc.printf("center button\r\n");
-					toggle_center_led();
+					//toggle_center_led();
+					if((GPIOB->ODR & (1<<10))==1){
+						pc.printf("center led off\r\n");
+						center_led_off();
+					}else if((GPIOB->ODR & (1<<10))==0){
+						pc.printf("center led on\r\n");
+						center_led_on();
+					}
 					rgb_red_off();
 					rgb_blue_off();
 					rgb_green_on();
@@ -125,6 +131,7 @@ int main(){
 					rgb_green_off();
 					rgb_red_on();
 					SYSTEM_ON();
+					WIND_ON(wind_power);
 				}
 			}   
 			wait(0.5);
@@ -152,23 +159,6 @@ void delay(uint32_t time){
    while(time--);
 }
 
-void RCC_Config(){
-   RCC->CR   |=   (1<<16);            // HSE ON
-  while(!(RCC->CR   &(1<<17)));   // HSE ready waiting
-  RCC->CFGR   |=   (1<<12);         // APB1 PSC=0
-  RCC->CFGR   &=   ~(7<<13);         // APB2 PSC=0
-  RCC->CFGR   |=   (10<<4);              // AHB   PSC=8
-  RCC->PLLCFGR   &=   ~(0xFFFFFFFF);   // APB2 PSC=0
-  RCC->PLLCFGR   |=   (1<<22);   //   HSE selected
-  RCC->PLLCFGR   &=   ~(3<<16);   // PLLP=2
-  RCC->PLLCFGR   |=   (64<<6);   // PLLN=64
-  RCC->PLLCFGR   |=   (1<<2);      // PLLM=4
-  RCC->PLLCFGR   |=   (1<<29);   // PLLR=2
-  RCC->PLLCFGR   |=   (1<<25);   // PLLQ=2
-   
-  RCC->CR   |=   (1<<24);            // PLL ON
-  while(!(RCC->CR   &(1<<25)));   // PLL ready waiting   
-}
 
 void GPIO_Config(){
   //LED : PA13(PA13), D6(PB10), A2(PA4)   => bit print
@@ -203,7 +193,6 @@ void GPIO_Config(){
 		//PC4 
 		GPIOC->MODER &= ~(1<<9);
 		GPIOC->MODER &= ~(1<<8);	
-	
 					 
 		 //RGB RED PA1 Output mode
 		 GPIOA->MODER &=~(1<<3);
@@ -216,7 +205,6 @@ void GPIO_Config(){
 		 //RGB GREEN PC6 Output mode
 		 GPIOC->MODER &= ~(1<<13);
 		 GPIOC->MODER |= (1<<12); 
-
 	
 		GPIOA->OTYPER=0;
 		GPIOA->OSPEEDR=0;
@@ -227,92 +215,21 @@ void GPIO_Config(){
    
 }
 
-void TIM_Config()
-{
-   //RGB Red: PA1(AF02:TIM5_CH2), Blue: PB0(AF02: TIM3_CH3), Green: PC6(AF02: TIM3_CH1)
-  RCC->APB1ENR   |=   (1<<1);         //   TIM3 clock enable
-  RCC->APB1ENR  |=  (1<<3);         //  TIM5 clock enable
-   
-  TIM3->PSC   |=   84-1;               //   Prescaler value
-  TIM5->PSC   |=  84-1;
-   
-  TIM3->ARR   = 1000;               //   Auto reload value
-  TIM5->ARR   = 1000;   
-   
-   // Green: TIM3_CH1
-  TIM3->CCMR1   |=   (3<<5);      //  PWM mode 1 - In upcounting, channel 1 is active as long as TIMx_CNT<TIMx_CCR1 else inactive.
-  TIM3->CCMR1   |=   (1<<3);      //  Preload register on TIMx_CCR1 enabled. Read/Write operations access the preload register. TIMx_CCR1 preload value is loaded in the active register at each update event.
-
-   // Blue: TIM3_CH3
-  TIM3->CCMR2   |=   (3<<5);
-  TIM3->CCMR2   |=   (1<<3);
-  
-   // Red: TIM5_CH2
-  TIM5->CCMR1   |=   (3<<13);
-  TIM5->CCMR1   |=   (1<<11);
-   
-   //TIM3
-  TIM3->CR1   |=   (1<<7);         //   TIMx_ARR register is buffered
-  //TIM5
-   TIM5->CR1   |=   (1<<7);
-  
-   //TIM3   
-  TIM3->EGR   |=   (1<<0);         //    Re-initialize the counter and generates an update of the registers.
-  //TIM5
-   TIM5->EGR   |=   (1<<0);
-   
-   // Green: TIM3_CH1
-  TIM3->CCER   &=   ~(1<<1);   //   OC1 active high
-  TIM3->CCER   &=   ~(1<<3);   //   CC1NP must be kept cleared in this case.
-  TIM3->CCER   |=   (1<<0);      //   On - OC1 signal is output on the corresponding output pin
-  
-   // Blue: TIM3_CH3
-  TIM3->CCER   &=   ~(1<<11);
-  TIM3->CCER   &=   ~(1<<9);
-  TIM3->CCER   |=   (1<<8);
-   
-   // Red: TIM5_CH2
-  TIM5->CCER   &=   ~(1<<5);
-  TIM5->CCER   &=   ~(1<<7);
-  TIM5->CCER   |=   (1<<4);
-   
-   // Green: TIM3_CH1
-  TIM3->DIER   |=   (1<<1);      //   capture/compare 1 interrupt enable
-  // Blue: TIM3_CH3
-   TIM3->DIER   |=   (1<<3);
-  // Red: TIM5_CH2
-   TIM5->DIER   |=   (1<<2);
-   
-  TIM3->SR   &=   ~(1<<1);      //   Capture interrupt flag clear
-  TIM5->SR   &=   ~(1<<1);
-   
-   // Green: TIM3_CH1
-  TIM3->CCR1   |=   500 ;         //   Duty cycle control value 
-  // Blue: TIM3_CH3
-   TIM3->CCR3   |=   500 ;
-  // Red: TIM5_CH2
-   TIM5->CCR2   |=   500 ;
-   
-  TIM3->CR1   |=   (1U<<0);      //   Run timer3
-  TIM5->CR1   |=   (1U<<0);  
-}
-
-
-
 void left_led_on(){
    GPIOA->ODR |= (1<<13);         
 }
+
 void left_led_off(){
    GPIOA->ODR &= ~(1<<13);
 }
+
 void toggle_mode(){
-	GPIOA->ODR ^= (1<<13); //xor operator
-	
-	if(GPIOA->ODR == 0){
+	if((GPIOA->ODR & (1<<13)) == 1){
 		pc.printf("Heater on\r\n");
+		left_led_off();
 		mode= HEAT;
-	}else if(GPIOA->ODR==1){
-		
+	}else if((GPIOA->ODR & (1<<13))==0){
+		left_led_on();
 		pc.printf("Cooler on\r\n");
 		mode = COOLER;
 	}	
@@ -324,18 +241,14 @@ void center_led_on(){
 void center_led_off(){
    GPIOB->ODR &= ~(1<<10);
 }
-void toggle_center_led(){
-	GPIOB->ODR ^= (1<<10);
-}
+
 void right_led_on(){
    GPIOA->ODR |= (1<<4);         
 }
 void right_led_off(){
    GPIOA->ODR &= ~(1<<4);
 }
-void toggle_right_led(){
-	GPIOA->ODR ^= (1<<4); 
-}
+
 
 void display(){
 	//pc.printf("displayed\r\n");
@@ -370,9 +283,7 @@ void display(){
       myGUI.printf ("HVAC power is off\r\n");
    }
    
-   
    wait(0.1);
-   
    //myGUI.printf ("%ux%u OLED Display\r\n",myGUI.width(), myGUI.height());
    myGUI.display();
 }
@@ -396,20 +307,23 @@ void rgb_green_off(){
     GPIOC->ODR &=~(1<<6);
 }
 
-
 void SYSTEM_OFF(){
 	right_led_off();
 	sys_on = !sys_on;
+	
 	left_led_off();
 	mode=HEAT;
+	
 	center_led_off();
 	automatic=false;
 }
 void SYSTEM_ON(){
 	right_led_on();
 	sys_on = !sys_on;
+	
 	left_led_on();
 	mode=COOLER;
+	
 	center_led_on();
 	automatic=true;
 }
